214c3af5f0d61408fc47770089bf5b055c78a211
git-green <git-green@yandex.ru>
2015-12-05 13:12:21 +0200

    Create Picture EJB

--- src/main/java/ua/sumdu/greenberg/model/objects/Picture.java
+++ src/main/java/ua/sumdu/greenberg/model/objects/Picture.java
@@ -1,10 +1,32 @@
 package ua.sumdu.greenberg.model.objects;
 
-public class Picture {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * This is EJB
+ *
+ * @author GREEN
+ *
+ */
+@Entity
+@Table(name = "PICTURES")
+public class Picture implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Column(name = "PRODUCT_ID")
 	private int productID;
+	@Column(name = "NAME")
 	private String URL;
-	
+
+	/**
+	 * This is constructor for Picture
+	 *
+	 * @param productID = productID
+	 * @param URL = picture URL
+	 */
 	public Picture(int productID, String URL) {
 		this.setProductID(productID);
 		this.setURL(URL);
@@ -25,5 +47,5 @@ public class Picture {
 	public void setURL(String uRL) {
 		URL = uRL;
 	}
-	
+
 }
